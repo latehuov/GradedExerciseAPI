0f4887e059c9a1b1ddc111ae2d711737820f0229
lasse <t8hula00@students.oamk.fi>
2021-02-27 21:09:13 +0200

    thignns

--- components/db.js
+++ components/db.js
@@ -5,6 +5,7 @@ const connection = new Pool({
   host: 'ec2-54-87-34-201.compute-1.amazonaws.com',
   database: 'd8eem8koof13qk',
   password: '560d7243836e161b66bf1575dd286b0385f5ba7eaa81b0f77b50ee758b27a080',
-  port: 5432
+  port: 5432,
+  ssl : {rejectUnauthorized : false}
 })
 module.exports = connection;
