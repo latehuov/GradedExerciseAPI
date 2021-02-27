02a6cf2cfe35781d60fee8e1f16e3658360de3bd
lasse <t8hula00@students.oamk.fi>
2021-02-27 20:54:31 +0200

    thangs

--- components/db.js
+++ components/db.js
@@ -6,5 +6,6 @@ const connection = new Pool({
   database: 'd8eem8koof13qk',
   password: '560d7243836e161b66bf1575dd286b0385f5ba7eaa81b0f77b50ee758b27a080',
   port: 5432,
+  ssl: true
 })
 module.exports = connection;
